2f21ad9b75d9c5d9ed11ea92cd39a56f4a5711f3
DetectiveRainy <107191047+DetectiveRainy@users.noreply.github.com>
2023-06-05 00:06:31 +0800

    Update MainCharacter.h

--- Source/Slash/Public/Character/MainCharacter.h
+++ Source/Slash/Public/Character/MainCharacter.h
@@ -6,12 +6,43 @@
 #include "Character/CharacterBase.h"
 #include "MainCharacter.generated.h"
 
+
+class USpringArmComponent;
+class UCameraComponent;
+
 /**
  * 
  */
 UCLASS()
 class SLASH_API AMainCharacter : public ACharacterBase
 {
-	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AMainCharacter();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+protected:
+
+	virtual void BeginPlay() override;
+
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+	void Turn(float Value);
+	void LookUp(float Value);
+
+
+private:
+	UPROPERTY(VisibleAnywhere)
+		USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere)
+		UCameraComponent* ViewCamera;
 	
 };
